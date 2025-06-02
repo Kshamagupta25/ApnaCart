267fb648a165ee9722b80c8cb1dc5f3d6574283a
Kshama gupta <guptakshama2004@gmail.com>
2025-06-02 03:08:57 -0700

    Assests uploaded

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import logo from '../assets/logo.jpg'
 const Header = () => {
   return (
-    <header className='h-20 shadow-md'>
-         Logo
+    <header className='h-20 shadow-md sticky top-0'>
+         {/* {Logo} */}
+         <div>
+          <div>
+            <img
+                src={logo}
+                width={170}
+                height={60}
+                alt='logo'
+            />
+          </div>
+         </div>
     </header>
   )
 }

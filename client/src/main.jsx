ca63a7cc993ff7b4bef68eaed67bb2d19cdc48ff
Kshama gupta <guptakshama2004@gmail.com>
2025-06-02 09:22:42 -0700

    Login page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './route/index'
-// import { Provider } from 'react-redux'
-// import { store } from './store/store.js'
+import { Provider } from 'react-redux'
+import { store } from './store/store.js'
 
 createRoot(document.getElementById('root')).render(
-   <StrictMode>
+  // <StrictMode>
+  <Provider store={store}>
     <RouterProvider router={router}/>
-   </StrictMode>,
+  </Provider>
+  // </StrictMode>,
 )

e5f48c93e8c25220eb18d82a6ae1dd592494809f
Kshama gupta <guptakshama2004@gmail.com>
2025-06-07 11:19:40 -0700

    Home page and add to Cart files added

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import categoryRouter from './route/category.route.js'
 import uploadRouter from './route/upload.router.js'
 import subCategoryRouter from './route/subCategory.route.js'
 import productRouter from './route/product.route.js'
-//import cartRouter from './route/cart.route.js'
+import cartRouter from './route/cart.route.js'
 //import addressRouter from './route/address.route.js'
 //import orderRouter from './route/order.route.js'
 
@@ -41,7 +41,7 @@ app.use("/api/category",categoryRouter)
 app.use("/api/file",uploadRouter)
 app.use("/api/subcategory",subCategoryRouter)
 app.use("/api/product",productRouter)
-//app.use("/api/cart",cartRouter)
+app.use("/api/cart",cartRouter)
 //app.use("/api/address",addressRouter)
 //app.use('/api/order',orderRouter)
 
